c07117cd62b697938a7052b8ce7c3a4964c846d4
marzique <marzique@gmail.com>
2019-11-05 01:16:21 +0200

    rada scraper as submodule of app

--- main.py
+++
@@ -1,6 +0,0 @@
-from rada.scraper import laws_by_deputy, name_by_id
-
-deputy_id = 21165 
-# vakarchuk_id = 11120 
-
-print(f'{name_by_id(deputy_id)} подав {laws_by_deputy(deputy_id)} законопроектів')
---
+++ rating/laws.py
@@ -0,0 +1,12 @@
+from .rada.scraper import laws_by_deputy
+from .models import Deputy
+
+
+def refresh_deputies_laws_number():
+    """Refresh each deputy's submitted laws variable by scraping it from rada website"""
+    deputies = Deputy.objects.all()
+    for deputy in deputies:
+        deputy.laws = laws_by_deputy(deputy.rada_id)
+        deputy.save()
+    # set some last_updated_value as singleton model in db here
+
