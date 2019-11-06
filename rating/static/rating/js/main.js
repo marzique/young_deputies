cbecfe2b929f3662d77e441561cece83e6de37f6
tarn <marzique@gmail.com>
2019-11-06 20:59:21 +0200

    fill semicircles after pageload

---
+++ rating/static/rating/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function fill_semicircles(){
+        $('.semicircle').removeClass('init')
+    }
+
+    setTimeout(fill_semicircles, 300)
+})
