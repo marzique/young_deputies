fde7b031587d7ddf20e760dcadc01ed18f77d51a
Danny Tarn <marzique@gmail.com>
2019-11-08 17:48:29 +0200

    voting system mockup done

--- rating/views.py
+++ rating/views.py
@@ -1,26 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Deputy
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Deputy, UniqueUser
 from .helpers import get_usd_rate, user_ip
 import requests
 
 
+@csrf_exempt
 def index(request):
-    context = {}
-    context['usd_rate'] = get_usd_rate()
-    context['deputies'] = Deputy.objects.order_by('position_current')
     IP = user_ip(request)
-    # TODO: create new view for votes, and 
-    print(f'User IP: [{IP}]')
+    print(IP)
+    if request.is_ajax():
+        
+        pk = int(request.POST['pk'])
+        deputy = Deputy.objects.filter(pk=pk).first()
+        user = UniqueUser.objects.filter(ip=IP).first()
 
-    return render(request, 'rating/index.html', context)
+        if user:
+            if deputy.uniqueuser_set.filter(ip=IP).exists():
+                print('user exists, and he already voted for this deputy')
+            else:
+                print('user exists, but he is not voted for this deputy')
+        else:
+            # u = UniqueUser(ip=IP)
+            # u.save()
+            print('no user with such ip, create it and count vote for this deputy')
 
-# TODO:
-# def vote(request):
-#     if post:
-#         if users_IP not in Voter.objects:
-#             deputy_voted_for.votes += 1
-#             store users_IP in db
-#         else:
-#             just fake that user voted
+        print(pk)
+        response = {
+            'status': 'success'
+        }
+        return JsonResponse(response)
+    else:
+        context = {}
+        context['usd_rate'] = get_usd_rate()
+        context['deputies'] = Deputy.objects.order_by('position_current')
+
+        return render(request, 'rating/index.html', context)
    
--- static/rating/js/main.js
+++ static/rating/js/main.js
@@ -5,7 +5,35 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     $('.semicircle').addClass('init')
-    
-
     setTimeout(fill_semicircles, 300)
+
+
+    // toggle
+    $(".vote").on( "click", function(e) {
+        e.preventDefault()
+        // $(this).toggleClass('voted')
+        var pk = $(this).data('pk')
+
+        $.ajax({
+            type: 'POST',
+            async: true,
+            url: '/',
+            data: {
+            	pk: pk, 
+            },
+            success: function(data) {
+                var status = data['status']
+                if (status == 'success'){
+                    $(this).toggleClass('voted')
+                }
+                else{
+                    alert('You already voted!')
+                }
+            },
+            error: function(){
+            	alert("Не робе");
+            },
+            dataType: 'json',
+        });
+    });
 })
